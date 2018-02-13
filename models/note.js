46ed724fe405c5eb5f9b655cffb5d50fd78c9f6b
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-02-13 14:25:50 -0800

    Challange completed

--- models/note.js
+++ models/note.js
@@ -10,6 +10,23 @@ const noteSchema = mongoose.Schema({
     default: Date.now}
 });
 
+noteSchema.methods.serialize = function () {
+
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content
+  };
+};
+
+noteSchema.set('toObject', {
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
 const Note = mongoose.model('Note', noteSchema);
 
 module.exports = Note;
--- routes/notes.js
+++ routes/notes.js
@@ -15,60 +15,98 @@ const Note = require('../models/note');
 router.use(bodyParser.json());
 
 /* ========== GET/READ ALL ITEM ========== */
-router.get('/notes', (req, res, next) => {
-  mongoose.connect(MONGODB_URI)
-    .then(() => {
-      Note
-        .find()
-        .then(notes => {
-          res.json(notes);
-        })
-        .catch(err => {
-          console.error(err);
-          res.status(500).json({ message: 'Internal server error' });
-        });
+router.get('/notes', (req, res, next) => {   
+  Note
+    .find()
+    .then(notes => {
+      res.json(notes);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
     });
 });
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/notes/:id', (req, res, next) => {
-  mongoose.connect(MONGODB_URI)
-    .then(() => {
-      Note
-        .findById(req.params.id)
-        .then(notes => {
-          res.json(notes);
-        })
-        .catch(err => {
-          console.error(err);
-          res.status(500).json({ message: 'Internal server error' });
-        });
+  Note
+    .findById(req.params.id)
+    .then(notes => {
+      res.json(notes);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
     })
     .catch(next);
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/notes', (req, res, next) => {
-
-  console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({ id: 2 });
-
+  //Check if required fields present
+  const requiredFields = ['title', 'content'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  
+  Note
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+    })
+    .then(note => res.status(201).json(note.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    })
+    .catch(next);
 });
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/notes/:id', (req, res, next) => {
 
-  console.log('Update a Note');
-  res.json({ id: 2 });
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).json({ message: message });
+  }
 
+  const toUpdate = {};
+  const updateableFields = ['title', 'content'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Note
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+    .then(note => {
+      res.status(204).json(note.serialize()).end();
+    })
+    .catch(err => res.status(500).json({ message: 'Internal server error' }))
+    .catch(next);
 });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/notes/:id', (req, res, next) => {
-
-  console.log('Delete a Note');
-  res.status(204).end();
-
+  Note
+    .findById(req.params.id)
+    .then(() => {
+      Note
+        .findByIdAndRemove(req.params.id)
+        .then(note => res.status(204).end())
+        .catch(err => res.status(500).json({ message: 'Internal server error' }));
+    })
+    .catch(next);
 });
 
 module.exports = router;

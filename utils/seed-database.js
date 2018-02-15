bff747d0ac7fa6123eee19014637e8fe7eed0e2d
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-02-15 08:31:43 -0800

    Folder model and seed data added

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -5,14 +5,22 @@ mongoose.Promise = global.Promise;
 
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
+const Folder = require('../models/Folder');
 
 const seedNotes = require('../db/seed/notes');
+const seedFolders = require('../db/seed/folders');
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
     return mongoose.connection.db.dropDatabase()
       .then(result => {
         console.info(`Dropped Database: ${result}`);
+      });
+  })
+  .then(() => {
+    return Folder.insertMany(seedFolders)
+      .then(results => {
+        console.info(`Inserted ${results.length} Folders`);
       });
   })
   .then(() => {
